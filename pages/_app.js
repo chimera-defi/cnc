5c8732140b9db0856fdf07357292e85f827435fe
Chimera <81845287+chimera-defi@users.noreply.github.com>
2021-11-21 17:17:40 +0530

    Fix missing '

--- pages/_app.js
+++ pages/_app.js
@@ -39,7 +39,7 @@ function	AppWrapper(props) {
 	return (
 		<>
 			<Head>
-				<title>{'Matic yield}</title>
+				<title>{'Matic yield'}</title>
 				<link rel={'icon'} href={'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧠</text></svg>'} />
 				<meta httpEquiv={'X-UA-Compatible'} content={'IE=edge'} />
 				<meta name={'viewport'} content={'width=device-width, initial-scale=1'} />
